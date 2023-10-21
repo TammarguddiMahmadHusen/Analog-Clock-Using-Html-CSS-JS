f7d8c730cdd508f59ff1d101fe0a71b0263692ca
Tammarguddi Mahmad Husen <125346008+TammarguddiMahmadHusen@users.noreply.github.com>
2023-10-21 09:59:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+var hr = document.getElementById('hour');
+var min = document.getElementById('min');
+var sec = document.getElementById('sec');
+
+function displayTime(){
+    let date = new Date();
+    // Getting hours, min, secs from date
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+
+    let hRotation = 30*hh + mm/2;
+    let mRotation = 6*mm;
+    let sRotation = 6*ss;
+
+    hr.style.transform =  `rotate(${hRotation}deg)`;
+    min.style.transform = ` rotate(${mRotation}deg)`;
+    sec.style.transform = ` rotate(${sRotation}deg)`;
+}
+setInterval(displayTime,1000);
